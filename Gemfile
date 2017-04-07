43f9c4753b3c3a19400d7d8c4f23452b63dc6798
charvine <crossovermeister@gmail.com>
2017-04-07 22:12:07 +0800

    facebook function ajax

--- Gemfile
+++ Gemfile
@@ -44,7 +44,10 @@ end
 
 gem 'bootstrap-sass'
 
+gem 'devise'
+
 group :development do
+  gem 'faker'
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '~> 3.0.5'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 get "/jquery-1" => "pages#jquery_1"
@@ -7,5 +8,11 @@ get "/jquery-3" => "pages#jquery_3"
 get "/jquery-4" => "pages#jquery_4"
 get "/jquery-5" => "pages#jquery_5"
 
-root "pages#jquery_1"
+   resources :posts do
+     member do
+       post "like" => "posts#like"
+       post "unlike" => "posts#unlike"
+      end
+end
+root "posts#index"
 end
